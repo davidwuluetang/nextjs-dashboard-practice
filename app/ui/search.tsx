806ee80ddb3b33c2f4cd1f56151afd1e78895d25
David Tang <122414668+davidwuluetang@users.noreply.github.com>
2025-04-08 13:39:46 -0400

    update to chapter 12 content

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -22,7 +22,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }, 400);
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
